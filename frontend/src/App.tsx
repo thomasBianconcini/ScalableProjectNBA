76c658b48f5bdeb369d498d03f29b5d5a8f92c5d
alberto <garagnanialberto@gmail.com>
2024-06-02 16:49:20 +0200

    Added style

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,11 @@ import React, { useEffect } from 'react';
 import './App.css';
 import { useState } from 'react';
 import BasicSelect from "./components/basicSelect.tsx";
+import ButtonAppBar from "./components/appBar.tsx";
 import Button from '@mui/material/Button';
+import {Grid} from "@mui/material";
+import Typography from '@mui/material/Typography';
+import background from './wallpaperone.jpg';
 
 interface ISquadre {
   squadra1: string;
@@ -54,16 +58,34 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <BasicSelect change={changeTeam1}></BasicSelect>
-      <BasicSelect change={changeTeam2}></BasicSelect>
-      <Button variant="outlined" onClick={handleClick}>Get winner</Button>
-      {value && (
-          <div>
-            <p>Squadra 1: {value.squadra1}</p>
-            <p>Squadra 2: {value.squadra2}</p>
-          </div>
-      )}
+    <div className="App" style={{backgroundImage: `url(${background})`, backgroundSize: 'cover', height:'100vh'}}>
+      <ButtonAppBar></ButtonAppBar>
+      <Grid container spacing={2} paddingTop={10} paddingLeft={10} paddingRight={10}>
+        <Grid item xs={4}></Grid>
+        <Grid item xs={8}>
+          <Typography variant="h2">Choose teams</Typography>
+        </Grid>
+        <Grid item xs={4}></Grid>
+        <Grid item xs={4}>
+          <BasicSelect change={changeTeam1}></BasicSelect>
+        </Grid>
+        <Grid item xs={4}>
+          <BasicSelect change={changeTeam2}></BasicSelect>
+        </Grid>
+        <Grid item xs={4}></Grid>
+        <Grid item xs={8}>
+          <Button variant="outlined" onClick={handleClick}>Get winner</Button>
+        </Grid>
+        <Grid item xs={4}></Grid>
+        <Grid item xs={8}>
+          {value && (
+              <div>
+                <p>Squadra 1: {value.squadra1}</p>
+                <p>Squadra 2: {value.squadra2}</p>
+              </div>
+          )}
+        </Grid>
+      </Grid>
     </div>
   );
 }
